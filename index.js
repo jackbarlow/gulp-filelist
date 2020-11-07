bc0973ec25766561c227043b8a0db7fde0819c00
Jack Barlow <z25x27y31@gmail.com>
2020-11-07 23:08:21 +0400

    Update index.js
    When we use option destRowTemplate as function, we get an array. And when we use "join('')" on array we get a string whisout separator.
    If in formatter function we return some string, out variable fileList be like that ["string 1", "string 2", ...] and when we put contents to file, we get somethink like that "string 1string 2".
    If in formatter function we return some json object, we cant put correct json to file, because we use join('') on array when destRowTemplate is set.
    I think no need use join ever
    
    Sorry for my bad english

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ module.exports = function(out, options) {
 
     cb();
   }, function(cb) {
-    var buffer = (options.destRowTemplate) ? new Buffer(fileList.join('')) : new Buffer(JSON.stringify(fileList, null, '  '));
+    var buffer = new Buffer(JSON.stringify(fileList, null, '  '));
 
     var fileListFile = new File({
       path: out,
